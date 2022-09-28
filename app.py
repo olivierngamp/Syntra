226c1cf6559b8ebdd93f28dfbc3616339d2e1d55
Olivier Ngamp <olivierngamp@hotmail.com>
2022-09-28 20:41:42 +0200

    test

---
+++ app.py
@@ -0,0 +1,36 @@
+#list of integers
+#we start at 0 with counting
+
+lijst =  [1, 2, 5, 6, 2, 1]
+
+# print(lijst[0])
+# print(lijst[1])
+# print(lijst[2])
+# print(lijst[3])
+# print(lijst[4])
+# print(lijst[5])
+
+# for i in lijst:
+#     print(i)
+
+# lijst[0] += 3
+# lijst[1] += 3
+# lijst[2] += 3
+# lijst[3] += 3
+# lijst[4] += 3
+# lijst[5] += 3
+
+# lengteLijst = len(lijst) # heeft als lengte 6 want er zitte 6 el
+#lengteLijst = 6
+
+# for i in range(lengteLijst):
+#     lijst[i] += 3
+#     print(lijst[i])
+
+# print(lijst)
+
+for i in range(5):
+    if i != 3:
+        print(i)
+    else:
+        print("")
